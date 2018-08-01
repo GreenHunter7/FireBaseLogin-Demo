77a347d7c039a4622bfa473dad2fa6fe68ffe887
Mohamed hussien <--replace-all>
2018-08-01 20:21:06 +0200

    Initial Commit

---
+++ FireBaseLogin-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseLogin-Demo
+//
+//  Created by Mohamed Hussien on 01/08/2018.
+//  Copyright © 2018 Mohamed Hussien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
